9bc3c88b11ab56e50c8aad729fc78c2f8619befa
AyumiizZ <korrawitc@gmail.com>
2021-05-29 13:21:32 +0700

    ✨ add navbar and sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,16 @@
 <template>
-  <!-- <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar> -->
-
-  <!-- </v-app> -->
-  <!-- <v-app>
-  <v-main>
-    <SideBar > <v-card elevation="2" outlined></v-card>
-  </SideBar>
-    <v-container>
-      <router-view />
-    </v-container>
-      
-    </v-main>
-  </v-app> -->
   <v-app>
-    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent app width=200>
-      <v-list-item class="px-2">
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-        </v-list-item-avatar>
+    
+    <NavBar></NavBar>
+    
+  
+    <SideBar></SideBar>
 
-        <v-list-item-title>Korrawit C.</v-list-item-title>
-
-        <v-btn icon @click.stop="mini = !mini">
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
+    
     <!-- Sizes your content based upon application components -->
     <v-main>
       <!-- Provides the application the proper gutter -->
-      <v-container fluid>
+      <v-container fluid style="padding: 24px 32px">
         <!-- If using vue-router -->
         <router-view />
       </v-container>
@@ -89,27 +23,33 @@
 </template>
 
 <script>
-// import SideBar from "@/components/SideBar";
+import NavBar from '@/components/NavBar';
+import SideBar from "@/components/SideBar";
+// import TestChart from '@/components/TestChart.vue'
+// import QueryChart from '@/components/QueryChart.vue';
 export default {
   name: "App",
   components: {
+    NavBar,
     // HelloWorld,
-    // SideBar: SideBar
+    SideBar
+    // TestChart,
   },
   data: () => ({
     drawer: true,
     items: [
-      { title: "Dashboard", icon: "mdi-home-city" },
-      { title: "History", icon: "mdi-history" },
-      { title: "Statistics", icon: "mdi-chart-bar" },
-      { title: "Alert Control", icon: "mdi-home-city" },
-      { title: "My Account", icon: "mdi-account" },
-      { title: "Users", icon: "mdi-account-group-outline" },
-      
-      
-      
+      { title: "Dashboard", icon: "mdi-home-city", link: "/" },
+      { title: "History", icon: "mdi-history", link: "/history" },
+      { title: "Statistics", icon: "mdi-chart-bar", link: "/about" },
+      { title: "Alert Control", icon: "mdi-home-city", link: "/about" },
+      { title: "My Account", icon: "mdi-account", link: "/about" },
+      { title: "Users", icon: "mdi-account-group-outline", link: "/about" },
     ],
     mini: true,
   }),
 };
 </script>
+
+<style scoped>
+
+</style>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,21 +1,22 @@
 <template>
-  <v-card>
+  <div>
     <v-navigation-drawer
+      clipped
+      flat
       v-model="drawer"
       :mini-variant.sync="mini"
       permanent
+      app
+      width="200"
     >
       <v-list-item class="px-2">
         <v-list-item-avatar>
           <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
         </v-list-item-avatar>
 
-        <v-list-item-title>John Leider</v-list-item-title>
+        <v-list-item-title>Korrawit C.</v-list-item-title>
 
-        <v-btn
-          icon
-          @click.stop="mini = !mini"
-        >
+        <v-btn icon @click.stop="mini = !mini">
           <v-icon>mdi-chevron-left</v-icon>
         </v-btn>
       </v-list-item>
@@ -23,10 +24,12 @@
       <v-divider></v-divider>
 
       <v-list dense>
+        <!-- <router-link :to="item.link"> -->
         <v-list-item
           v-for="item in items"
           :key="item.title"
           link
+          :to="item.link"
         >
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
@@ -36,23 +39,31 @@
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        <!-- </router-link> -->
       </v-list>
     </v-navigation-drawer>
-  </v-card>
+  </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        drawer: true,
-        items: [
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
-        ],
-        mini: true,
-      }
-    },
-  }
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    // SideBar: SideBar
+    // TestChart,
+  },
+  data: () => ({
+    drawer: true,
+    items: [
+      { title: "Dashboard", icon: "mdi-home-city", link: "/" },
+      { title: "History", icon: "mdi-history", link: "/history" },
+      { title: "Statistics", icon: "mdi-chart-bar", link: "/about" },
+      { title: "Alert Control", icon: "mdi-home-city", link: "/about" },
+      { title: "My Account", icon: "mdi-account", link: "/about" },
+      { title: "Users", icon: "mdi-account-group-outline", link: "/about" },
+    ],
+    mini: true,
+  }),
+};
 </script>
